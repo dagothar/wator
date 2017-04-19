6ebd6958c39607fad323c10032e9fdf376f5d979
dagothar <dagothar@gmail.com>
2017-04-19 22:11:50 +0200

    initial values

--- scripts/app.js
+++ scripts/app.js
@@ -22,10 +22,10 @@ var App = (function() {
     $('.button-start').show();
     $('.button-stop').hide();
     $('.slider-speed').val(0);
-    $('.parameter-width').val(160);
-    $('.parameter-height').val(120);
+    $('.parameter-width').val(100);
+    $('.parameter-height').val(100);
     $('.parameter-initial-prey').val(5000);
-    $('.parameter-initial-predators').val(1000);
+    $('.parameter-initial-predators').val(500);
     $('.parameter-prey-reproduction').val(10);
     $('.parameter-predator-reproduction').val(25);
     $('.parameter-predator-starvation').val(10);
